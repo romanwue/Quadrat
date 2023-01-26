de951f17ec587d1e0019ae0dc2f1e81d474cd488
Roman Wüger <office@wueger.at>
2023-01-26 18:03:21 +0100

    Initial commit

---
+++ src/main/java/com/fhtw/quadrat/QuadratApplication.java
@@ -0,0 +1,13 @@
+package com.fhtw.quadrat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuadratApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuadratApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fhtw/quadrat/QuadratApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fhtw.quadrat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuadratApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
